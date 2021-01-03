d556aef0630a98e01d7a3ab2b7e9d11e54fa7e47
Jo <jo-lion@hotmail.fr>
2021-01-03 17:17:08 +0100

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,16 +34,16 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    'nuxt-imagemin'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    'bootstrap-vue/nuxt'
     // Simple usage
     // '@nuxtjs/component-cache',
-    'nuxt-imagemin'
   ],
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {

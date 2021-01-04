a77db033e9198b3860ce83463ff8c96be00a6785
Jo <jo-lion@hotmail.fr>
2021-01-04 10:06:58 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,14 @@ export default {
     Une,
     Actualites,
     Footer
+  },
+
+  head () {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+    }
   }
+
 }
 </script>
 
